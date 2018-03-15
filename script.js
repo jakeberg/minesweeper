7f4ac59cf0f579095b8b05fa1b417c4dccce18b8
Jake Berg <jacobvictorberg@gmail.com>
2018-03-15 12:25:05 -0400

    Can now detect either an open tile or a bomb

--- script.js
+++ script.js
@@ -9,10 +9,22 @@
 
 const main = document.querySelector("main");
 const tileImages = ["assets/tile.jpg", "assets/tileEmpty.jpg", "assets/bomb.jpg", "assets/flag.png"];
+const tilesArray = [];
+const bombArray = [];
 
-function CreateGameBoard(frame_images, target_div, x, y) {
+
+// Creates an array of ten random number to place bombs on board
+for (let i = 0; i <= 10; i++) {
+    let randomNumber = Math.floor(Math.random() * Math.floor(100));
+    bombArray.push(randomNumber)
+};
+
+console.log(bombArray)
+
+//Object constructor of a tile and it's click attributes
+function Tile(frame_images, target_div, x, y, num) {
     this.images = frame_images;
-    this.frame = 1;
+    // this.frame = 1;
     this.img = document.createElement("img");
     this.img.src = this.images[0];
     this.img.style.position = "absolute";
@@ -20,34 +32,43 @@ function CreateGameBoard(frame_images, target_div, x, y) {
     this.img.style.height = "50px";
     this.img.style.left = x + "px";
     this.img.style.top = y + "px";
+    var tileNumber = parseInt(num);
+    tilesArray.push(num)
 
     target_div.appendChild(this.img);
 
     var clickedImage = this.img;
     var newImage = this.images;
     var newFrameCount = this.frame;
-    this.tileSelection = function () {
-        if (newFrameCount < 3) {
-            clickedImage.src = newImage[newFrameCount];
-            newFrameCount++;
-        }
-    }
 
     this.flagBomb = function (event) {
         event.preventDefault();
         clickedImage.src = newImage[3];
     }
 
-    this.img.addEventListener("click", this.tileSelection);
+    this.checkBomb = function () {
+        for (let i = 0; i < bombArray.length; i++) {
+            if (tileNumber == bombArray[i]) {
+                clickedImage.src = newImage[2];
+                alert("You Lose!")
+            } else {
+                clickedImage.src = newImage[1];
+            }
+        }
+    }
     this.img.addEventListener('contextmenu', this.flagBomb);
+    this.img.addEventListener("click", this.checkBomb);
 }
 
+
+// Nested loops that make the game board
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         var x = i * 50;
         var y = j * 50;
-        var board = new CreateGameBoard(tileImages, main, x, y);
+        var number = String(i) + String(j);
+        var tile = new Tile(tileImages, main, x, y, number);
     }
 }
 
-
+console.log(main);
