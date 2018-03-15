53e80d1eec4c81918871efc7023bd6b55424beee
Jake Berg <jacobvictorberg@gmail.com>
2018-03-15 17:48:39 -0400

    you can resize board, reset game, and click and mark the right tiles

--- script.js
+++ script.js
@@ -8,60 +8,66 @@
 var difficulty = 8;
 var bombs = 8;
 const easy = document.getElementById("easy").addEventListener("click", function () {
+    gameBoard.innerHTML = "";
     difficulty = 8;
     bombs = 8;
     createBoard();
 });
 const medium = document.getElementById("medium").addEventListener("click", function () {
+    gameBoard.innerHTML = "";
     difficulty = 10;
     bombs = 10;
     createBoard();
 });
 const hard = document.getElementById("hard").addEventListener("click", function () {
+    gameBoard.innerHTML = "";
     difficulty = 15;
     bombs = 15;
     createBoard();
 });
 
-
-const main = document.getElementById("gameBoard")
+// Variables
+const resetButton = document.getElementById("reset").addEventListener("click", reset);
+const gameBoard = document.getElementById("gameBoard")
 const tileImages = ["assets/tile.jpg", "assets/tileEmpty.jpg", "assets/bomb.jpg", "assets/flag.png"];
-// const tilesArray = [];
-let bombArray = [];
+const bombPositionArray = [];
+let randomNumberArray = [];
+let coordinatesArray = [];
 
 // Creates an array of ten random number to place bombs on board
 for (let i = 0; i <= bombs; i++) {
     let randomNumber = Math.floor(Math.random() * Math.floor(difficulty * difficulty));
-    bombArray.push(randomNumber)
+    randomNumberArray.push(randomNumber)
 };
 
 //Object constructor of a tile and it's click attributes
-function Tile(frame_images, target_div, x, y, num) {
+function Tile(tileImages, board, x, y, num) {
+
     this.img = document.createElement("img");
-    this.img.src = frame_images[0];
+    this.img.src = tileImages[0];
+    this.img.value = parseInt(num);
     this.img.style.position = "absolute";
     this.img.style.width = "50px";
     this.img.style.height = "50px";
     this.img.style.left = x + "px";
     this.img.style.top = y + "px";
-    // tilesArray.push(num);
-    target_div.appendChild(this.img);
+    board.appendChild(this.img);
 
-    var tileNumber = parseInt(num);
     var clickedImage = this.img;
     var newImage = this.images;
     var newFrameCount = this.frame;
-
+    var tileNumber = parseInt(num);
 
     checkTile = function () {
-        console.log(tileNumber)
-        if (bombArray.includes(tileNumber)) {
-            clickedImage.src = frame_images[2];
+        console.log(tileNumber);
+        if (randomNumberArray.includes(tileNumber)) {
+            clickedImage.src = tileImages[2];
+            document.getElementById("message").style.display = "block";
         } else {
-            clickedImage.src = frame_images[1];
+            clickedImage.src = tileImages[1];
         }
-
     }
+
     flagBomb = function (event) {
         event.preventDefault();
         clickedImage.src = newImage[3];
@@ -70,19 +76,39 @@ function Tile(frame_images, target_div, x, y, num) {
     this.img.addEventListener("click", checkTile);
     this.img.addEventListener('contextmenu', flagBomb);
 }
-console.log(bombArray)
+
 
 // Nested loops that make the game board
 function createBoard() {
+    let number = 0;
     for (let i = 0; i < difficulty; i++) {
+        
         for (let j = 0; j < difficulty; j++) {
-            var x = i * 50;
-            var y = j * 50;
-            var number = String(i) + String(j);
-            var tile = new Tile(tileImages, main, x, y, number);
+            var x = j * 50;
+            var y = i * 50;
+            number++;
+            var tile = new Tile(tileImages, gameBoard, x, y, number);
         }
     }
 }
 createBoard();
 
-console.log(main)
+// This makes a table where you can keep track of the bomb positions and the tile numbers
+var count = 0;
+for (let i = 0; i < difficulty; i++) {
+    let position = [];
+    let coordinates = [];
+    for (let j = 0; j < difficulty; j++) {
+        count++;
+        coordinates.push(count);
+        if (randomNumberArray.includes(count)) {
+            position.push(count);
+        } else {
+            position.push(null);
+        }
+    }
+    bombPositionArray.push(position)
+    coordinatesArray.push(coordinates)
+}
+console.table(bombPositionArray)
+console.table(coordinatesArray)
